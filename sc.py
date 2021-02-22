e5156c45f100643175e867fb22d32d8060a695cd
MAJDGHUN <79091018+MAJDGHUN@users.noreply.github.com>
2021-02-22 23:24:55 +0100

    Add files via upload

--- sc.py
+++ sc.py
@@ -4,8 +4,10 @@ import numpy as np
 from bs4 import BeautifulSoup
 import requests
 import time
-import os
+import pymongo as mongo
 
+
+client = mongo.MongoClient("mongodb://127.0.0.1:27017")
 
 
   
@@ -22,8 +24,8 @@ def get_data():
         #print(d)
         Hash = d.find('a', attrs={'class':'sc-1r996ns-0 fLwyDF sc-1tbyx6t-1 kCGMTY iklhnl-0 eEewhk d53qjk-0 ctEFcK'})
         Time = d.findAll('span', attrs={'class':'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
-        btc2 = Time
-        usd3= Time
+        btc2 = d.findAll('span', attrs={'class':'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
+        usd3= d.findAll('span', attrs={'class':'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
        
 
         all1=[]
@@ -67,21 +69,35 @@ def get_data():
 
 
 
+  
+db = client["My_DB"]
+c_crypto = db["My_Col"]
 
 
 while True:
   time.sleep(60)
+
+
   get_data()
   results = []
   results.append(get_data())
   flatten = lambda l: [item for sublist in l for item in sublist]
   df = pd.DataFrame(flatten(results),columns=['Hash', 'Time', 'Amount(BTC)', 'Amount(USD)'])
   df = df.sort_values('Amount(USD)', ascending=False)
-  print(df.head(1))
-  df.head(1).to_csv("logfile.txt", header=None, index=None, sep='\t', mode='a')
+
+  hasht =df['Hash'].iloc[0]
+  timet =df['Time'].iloc[0]
+  btct =df['Amount(BTC)'].iloc[0]
+  usdt =df['Amount(USD)'].iloc[0]
+
+  My_Crypto = {"Hash": hasht, "Time" : timet, "Amount(BTC)" : btct, "Amount(USD)" : usdt}
+  c_crypto.insert_one(My_Crypto)
 
 
 
+
+  print(hasht,timet,btct,usdt)
+  df.head(0).to_csv("logfile.txt", header=None, index=None, sep='\t', mode='a')
 
   
   
